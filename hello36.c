a6a7291774936e232a51b1266cb6b4e49d0bb802
Shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-02-25 22:35:36 +0530

    day6:Counting  odd number for n values

---
+++ hello36.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,a[50],count=0;
+    for(i=0;i<50;i++){
+if(i%2!=0){
+    printf("odd:%d\n",i);
+    count++;
+}
+
+    }
+    printf("total.%d\n",i);
+    return 0;
+    
+}
